726e02985feb243543eb60f37444795d18689c63
Dan Wahlin <danwahlin@Dans-MacBook-Pro.local>
2017-05-23 12:05:49 -0400

    Add async/await and Task<T> example

---
+++ AsyncAwaitTaskT/AsyncAwaitTaskT/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitTaskT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetContent();
+            Console.ReadLine();
+        }
+
+        static async void GetContent()
+        {
+            var http = new HttpWrapper();
+            var content = await http.MakeRequest("http://microsoft.com");
+            Console.WriteLine(content);
+        }
+
+    }
+
+    class HttpWrapper
+    {
+        public async Task<string> MakeRequest(string url)
+        {
+            HttpClient http = new HttpClient();
+            var response = await http.GetAsync("http://microsoft.com");
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
