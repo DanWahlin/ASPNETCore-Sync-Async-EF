2dbe12ba4d04e269831be3c643fc836e697e2121
Dan Wahlin <danwahlin@Dans-MacBook-Pro.local>
2017-05-23 15:20:40 -0400

    Added WriteLine

--- AsyncAwaitTaskT/AsyncAwaitTaskT/Program.cs
+++ AsyncAwaitTaskT/AsyncAwaitTaskT/Program.cs
@@ -8,25 +8,32 @@ namespace AsyncAwaitTaskT
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Main Start");
             GetContent();
+            Console.WriteLine("Main End");
+
             Console.ReadLine();
         }
 
         static async void GetContent()
         {
+            Console.WriteLine("GetContent() Start");
             var http = new HttpWrapper();
-            var content = await http.MakeRequest("http://microsoft.com");
-            Console.WriteLine(content);
+            var content = await http.GetUrlString("http://microsoft.com");
+            Console.WriteLine(Environment.NewLine +content);
+            Console.WriteLine("GetContent() End");
         }
 
     }
 
     class HttpWrapper
     {
-        public async Task<string> MakeRequest(string url)
+        public async Task<string> GetUrlString(string url)
         {
+            Console.WriteLine("HttpWrapper.GetUrlString() Start");
             HttpClient http = new HttpClient();
             var response = await http.GetAsync("http://microsoft.com");
+            Console.WriteLine("HttpWrapper.GetUrlString() End");
             return await response.Content.ReadAsStringAsync();
         }
     }
